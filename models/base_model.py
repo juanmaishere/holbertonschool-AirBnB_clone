67621569ca23ff0c8756bd981f23806fed5181e8
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-23 19:50:40 +0000

    feat new airbnb clone

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Definition of baseclass for other classes
+"""
+import uuid
+import datetime
+
+
+class BaseModel:
+    """
+    Basemodel for other classes sharing same attr
+    Id uniq id for the class
+    Created and updated , date records
+    """
+    def __init__(self):
+        """
+        id attribute unique for each instance
+        created at and updated at , date and time of creation/update
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """
+        Prints a string of the attrs Basemodel
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Update to new datetime when change made
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Dictionary of instances
+        each attribute displayed
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
