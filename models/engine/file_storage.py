e56f10e2912346e584b8f70e704c161e80317095
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-25 13:36:04 -0300

    feat

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 """ summary """
+import json
 
 
 class FileStorage():
+    """
+    Class filestorage
+    made for storing the dicts
+    in json
+    """
+    
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self, obj):
+        return self.__objects
+    
+    def save(self):
+        with open(self.__file_path, 'w') as f:
+            c = json.dumps(self.__objects)
+            f.write(c)
