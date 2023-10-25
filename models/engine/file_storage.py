eef35b392a3a3c2d055991b898033dbae120aee2
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-25 14:35:06 -0300

    featg

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,21 +3,27 @@
 import json
 
 
-class FileStorage():
-    """
-    Class filestorage
-    made for storing the dicts
-    in json
-    """
+class FileStorage:
     
     def __init__(self):
         self.__file_path = "file.json"
         self.__objects = {}
 
-    def all(self, obj):
+    def all(self):
         return self.__objects
-    
+
     def save(self):
         with open(self.__file_path, 'w') as f:
-            c = json.dumps(self.__objects)
-            f.write(c)
+            for key, value in self.__objects.items():
+                json.dump({key: value}, f)
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(f)
+        except Exception:
+            pass
